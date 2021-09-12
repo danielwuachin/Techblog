64394b1c6cc0b2c3613904cc4c1abe77a30d26e9
danielwuachin <j.d.rodriguez.1624@gmail.com>
2021-09-11 20:05:07 -0400

    creada y terminada modelo y clase para Plataformas

---
+++ back/clases/plataformas.class.php
@@ -0,0 +1,232 @@
+<?php
+
+require_once "conexion/conexion.php";
+require_once "respuestas.class.php";
+require_once "helpers.class.php";
+
+class Plataformas extends conexion{
+
+    
+    private $table = "plataformas";
+    private $id = "";
+    private $plataforma = "";
+    private $token = "";
+
+
+
+    #PARA EL POST-----------  HACER CREATE
+    public function post($json){
+        $_helpers = new Helpers;
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+
+
+        #para comprobar si enviaron el token
+        if (!isset($datos['token'])) {
+            return $_respuestas->error_401(); 
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken = $_helpers->buscarToken($this->token);
+            if ($arrayToken) {
+
+
+                /* COMPROBAMO SI ES ADMIN */
+                $token = $this->token;
+                $is_admin = $_helpers->isAdmin($token);
+                $admin_verify = $is_admin[0]['ROLE'];
+                var_dump($is_admin);
+
+                if($admin_verify != 'admin'){
+                    return $_respuestas->error_401("area solo para administradores, no tienes permisos suficioentes");
+                }else{
+
+                    #comprobamos si todos los datos requeridos nos llegaron
+                    if (!isset($datos['plataforma'])) {
+                        return $_respuestas->error_400();
+                    }else{
+    
+                        $conexion = $this->conexion;
+                        /* var_dump($conexion);die(); */
+                        #estos se dejan asi ya que en el if de arriba se confirma su existencia
+                        $this->plataforma = mysqli_real_escape_string($conexion, $datos['plataforma']);
+                        
+    
+                        $resp = $this->insertarPlataforma();
+                        /* var_dump($resp); */
+                        if ($resp) {
+                            $respuesta = $_respuestas->response;
+                            $respuesta['result'] = array (
+                                "id" => $resp
+                            );
+                            return $respuesta;
+                        }else{
+                            return $_respuestas->error_500();
+                        }
+                    }
+                }
+
+            }else{
+                return $_respuestas->error_401("el token que se envio es invalido o caduco");
+            }
+        }
+    }
+
+
+
+    private function insertarPlataforma(){
+        $query = "INSERT INTO " . $this->table ." (plataforma) 
+        VALUES
+        ('" . $this->plataforma . "') ";
+        $resp = parent::nonQueryId($query);
+        /* var_dump($query); */
+        if ($resp) {
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+
+    #PARA HACER UPDATE-------- ----------------------------METODO PUT
+    public function put($json){
+        $_helpers = new Helpers;
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+
+
+
+        #para comprobar si enviaron el token!!!!
+        if (!isset($datos['token'])) {
+            return $_respuestas->error_401(); 
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken = $_helpers->buscarToken($this->token);
+            if ($arrayToken) {
+
+
+                /* COMPROBAMO SI ES ADMIN */
+                $token = $this->token;
+                $is_admin = $_helpers->isAdmin($token);
+                $admin_verify = $is_admin[0]['ROLE'];
+                var_dump($is_admin);
+
+                if($admin_verify != 'admin'){
+                    return $_respuestas->error_401("area solo para administradores, no tienes permisos suficioentes");
+                }else{
+                    #comprobamos si todos los datos requeridos nos llegaron
+                    if (!isset($datos['id'])) {
+                        return $_respuestas->error_400();
+                    }else{
+    
+                        $this->id = $datos["id"];
+                        $conexion = $this->conexion;
+    
+                        $this->plataforma = mysqli_real_escape_string($conexion, $datos['plataforma']);
+    
+                            #EJECUTAR FUNCION GAURDAR CON LOS PARAMETROS RECIEN GUARDADOS ARRIBA
+                            $resp = $this->modificarPlataforma();
+                            var_dump($resp);
+                            if ($resp) {
+                                $respuesta = $_respuestas->response;
+                                $respuesta['result'] = array (
+                                    "id" => $resp
+                                );
+                                return $respuesta;
+                            }else{
+                                return $_respuestas->error_500();
+                            }
+                    }
+                }
+            }else{
+                return $_respuestas->error_401("el token que se envio es invalido o caduco");
+            }
+        }
+    }
+
+
+    
+    private function modificarPlataforma(){
+        
+        $query = "UPDATE " . $this->table ." SET plataforma = '" . $this->plataforma . "'
+        WHERE id = '" . $this->id . "'";
+        
+        $resp = parent::nonQuery($query);
+        
+        #COMO NONQUERY DEVUELVE LAS FILAS AFECTADAS, SI ES IGUAL O MAYOR A UNO ES QEU SI FUNCIONO
+        if ($resp >= 1) {
+            return $resp;
+        }else{
+            return 0;
+        }
+    }
+
+
+
+    #PARA BORRARR    --------------------------------------------------------------
+    public function delete($json){
+        $_helpers = new Helpers;
+        $_respuestas = new respuestas;
+        $datos = json_decode($json, true);
+
+        #para comprobar si enviaron el token
+        if (!isset($datos['token'])) {
+            return $_respuestas->error_401(); 
+        }else{
+            $this->token = $datos['token'];
+            $arrayToken = $_helpers->buscarToken($this->token);
+            if ($arrayToken) {
+                
+
+                /* COMPROBAMO SI ES ADMIN */
+                $token = $this->token;
+                $is_admin = $_helpers->isAdmin($token);
+                $admin_verify = $is_admin[0]['ROLE'];
+                var_dump($is_admin);
+
+                if($admin_verify != 'admin'){
+                    return $_respuestas->error_401("area solo para administradores, no tienes permisos suficioentes");
+                }else{
+                    #comprobamos si todos los datos requeridos nos llegaron
+                    if (!isset($datos['id'])) {
+                        return $_respuestas->error_400();
+                    }else{
+                        #como se recibe es el id del campo a actualizar, se guarda en una variable y el resto se verifica aparte
+                        $this->id = $datos['id'];
+    
+    
+                        #EJECUTAR FUNCION GAURDAR CON LOS PARAMETROS RECIEN GUARDADOS ARRIBA
+                        $resp = $this->eliminarPlataforma();
+                        if ($resp) {
+                            $respuesta = $_respuestas->response;
+                            $respuesta['result'] = array (
+                                "id" => $this->id
+                            );
+                            return $respuesta;
+                        }else{
+                            return $_respuestas->error_500();
+                        }
+                    } 
+
+                }
+            }else{
+                return $_respuestas->error_401("el token que se envio es invalido o caduco");
+            }
+        }
+    }
+
+
+    private function eliminarPlataforma(){
+        $query = "DELETE FROM ". $this->table ." WHERE id = '" . $this->id . "'";
+        $resp = parent::nonQuery($query);
+
+        if ($resp >= 1) {
+            return $resp; 
+        }else{
+            return 0;
+        }
+    }
+}
+?>
+
+
---
+++ back/plataformas.php
@@ -0,0 +1,151 @@
+<?php
+require_once "clases/plataformas.class.php";
+require_once "clases/respuestas.class.php";
+
+
+#instanciar clases, se usa el _ para saber que la variable es la instancia de una clase
+$_plataformas = new Plataformas;
+$_respuestas = new respuestas;
+$_helpers = new Helpers;
+$tabla = 'plataformas';
+
+#para los READ, YA SEAN todos los plataformas o solo uno
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['page'])) {
+        $pagina = $_GET['page'];
+        $listaPlataformas = $_helpers->listar($pagina, $tabla);
+
+        #esto se manda siempre a la cabecera como una respuesta adecuada
+        header("Content-Type: application/json");
+
+        #convertir json
+        echo json_encode($listaPlataformas);
+
+        #esto se manda siempre a la cabecera como una respuesta adecuada
+        http_response_code(200);
+
+
+    }elseif(isset($_GET['id'])){
+        $plataformaId = $_GET['id'];
+        $datosPlataforma = $_helpers->obtener($plataformaId, $tabla);
+
+        #esto se manda siempre a la cabecera como una respuesta adecuada
+        header("Content-Type: application/json");
+
+        #convertir json
+        echo json_encode($datosPlataforma);
+
+        #esto se manda siempre a la cabecera como una respuesta adecuada
+        http_response_code(200);
+    }else{
+        $allData = $_helpers->obtenerAll($tabla);
+        #esto se manda siempre a la cabecera como una respuesta adecuada
+        header("Content-Type: application/json");
+
+        #convertir json
+        echo json_encode($allData);
+
+        #esto se manda siempre a la cabecera como una respuesta adecuada
+        http_response_code(200);
+    }
+
+
+
+
+
+    #para guardar datos CREATE
+}else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    #recibir los datos enviados por el POST
+    $postBody = file_get_contents("php://input");
+
+    #enviamos esto al manejador
+    $datosArray = $_plataformas->post($postBody);
+    
+    #DEVOLVEMOS UNA RESPUESTA AL FRONTEND
+    header("Content-Type: application/json");
+    if (isset($datosArray['result']['error_id'])) {
+        $responseCode = $datosArray['result']['error_id'];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    #aqui gracias al json_encode se pasa el array a string
+    echo json_encode($datosArray);
+
+
+
+
+    #actualizar
+}else if ($_SERVER['REQUEST_METHOD'] == 'PUT'){
+    #recibir los datos enviados por el POST
+    $postBody = file_get_contents("php://input"); 
+    
+    #enviamos datos al manejador
+    $datosArray = $_plataformas->put($postBody);
+
+    #DEVOLVEMOS UNA RESPUESTA AL FRONTEND
+    header("Content-Type: application/json");
+    if (isset($datosArray['result']['error_id'])) {
+        $responseCode = $datosArray['result']['error_id'];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    #aqui gracias al json_encode se pasa el array a string
+    echo json_encode($datosArray);
+
+
+
+
+
+}else if ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
+
+
+    #RECIBIR LOS DATOS POR EL HEADER-------- si el frontend es con vuejs, los enviara por ahi
+    $headers = getallheaders();
+    /* print_r($headers);die(); */
+    if (isset($headers['token']) && isset($headers['id'])) {
+        #recibimos los datos por el header
+        $send = [
+            "token" => $headers['token'],
+            "id" => $headers['id']
+        ];
+        #ahora lo convertimos a un JSON para que sea usado
+        
+        $postBody = json_encode($send);
+    }else{
+        #recibir los datos enviados por el POST
+        $postBody = file_get_contents("php://input"); 
+
+    }
+
+
+
+    
+        #enviamos datos al manejador
+    $datosArray = $_plataformas->delete($postBody);
+
+    #DEVOLVEMOS UNA RESPUESTA AL FRONTEND
+    header("Content-Type: application/json");
+    if (isset($datosArray['result']['error_id'])) {
+        $responseCode = $datosArray['result']['error_id'];
+        http_response_code($responseCode);
+    }else{
+        http_response_code(200);
+    }
+    #aqui gracias al json_encode se pasa el array a string
+    echo json_encode($datosArray); 
+
+
+
+
+
+
+
+}else{
+
+    header("Content-Type: application/json");
+    $datosArray = $_respuestas->error_405();
+    echo json_encode($datosArray);
+}
