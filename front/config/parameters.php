3856fb48579b2268d58e72102469bd4d017ae3d7
danielwuachin <j.d.rodriguez.1624@gmail.com>
2022-01-22 16:36:50 -0400

    comenzando de cero el frontend

--- front/config/parameters.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//esto es para literalmente definir constantes o valores
-define("base_url", "http://localhost/cursos/php-api/api/front/");
-define("controller_default", "home_controller");
-define("action_default", "index");
